7575dd4dc8cd5239ee67ed765ee8ab59eb171379
rlaalsdn <tnfalsdn12@naver.com>
2025-02-18 15:51:23 +0900

    Init(*): 초기 프로젝트 설정

---
+++ src/main/java/kr/co/green/SpringProject5Application.java
@@ -0,0 +1,13 @@
+package kr.co.green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject5Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/green/SpringProject5ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
