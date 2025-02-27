ace846e6c0b5f3dd7a8eccfd2a6dc15334130d8a
yoonkimin <sked4824@gmail.com>
2025-02-27 17:09:23 +0900

    Init(*): Git 초기설정

---
+++ src/test/java/kr/kro/bbanggil/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.bbanggil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
