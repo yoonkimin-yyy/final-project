3b418afd1360e6437e3db21319821a9fe2bc1b88
rlaalsdn <tnfalsdn12@naver.com>
2025-03-18 12:07:09 +0900

    Fix(member): 회원가입 페이지 이동, 핸드폰 본인인증 코드 alert

--- src/main/java/kr/kro/bbanggil/member/controller/MemberController.java
+++ src/main/java/kr/kro/bbanggil/member/controller/MemberController.java
@@ -33,7 +33,7 @@ public class MemberController {
     // 회원가입 타입 선택 페이지 (일반/사업자 선택)
     @GetMapping("/typeloginup/form")
     public String typeLoginup() {
-        return "common/type-loginup";
+        return "/common/type-loginup";
     }
 
     // 체크박스 페이지 (일반/사업자 구분)
@@ -41,7 +41,7 @@ public class MemberController {
     public String checkBox(@RequestParam(value = "type", required = false, defaultValue = "user") String type, Model model) {
         model.addAttribute("userType", type);
         model.addAttribute("checkBoxDto", new MemberRequestCheckBoxDto()); // 체크박스 DTO 추가
-        return "common/checkbox";
+        return "/common/checkbox";
     }
     
     // 체크박스 
@@ -55,7 +55,7 @@ public class MemberController {
         if (!"Y".equals(checkBoxDto.getTermsofuse()) || !"Y".equals(checkBoxDto.getInformation())) {
             model.addAttribute("error", "필수 항목을 체크해야 합니다.");
             model.addAttribute("userType", type);
-            return "common/checkbox";
+            return "/common/checkbox";
         }
 
         // 세션에 체크박스 정보 저장
@@ -81,7 +81,7 @@ public class MemberController {
         }
 
 
-        return "user/loginup";
+        return "/user/loginup";
     }
 
     // 일반 회원가입 처리
@@ -127,7 +127,7 @@ public class MemberController {
             model.addAttribute("checkBoxDto", checkBoxDto);
         }
 
-        return "owner/business-loginup";
+        return "/owner/business-loginup";
     }
 
     // 사업자 회원가입 처리
@@ -159,7 +159,7 @@ public class MemberController {
     	    
     	 // 로그인 페이지 진입 시 에러 메시지 초기화
     	 session.removeAttribute("status");  
-    	 return "common/loginin";  
+    	 return "/common/loginin";  
     }
     
     // 로그인 처리
@@ -185,7 +185,7 @@ public class MemberController {
     // 아이디/비밀번호 찾기 페이지
     @GetMapping("/findidpw")
     public String findIdPw() {
-        return "common/find-id-pw";
+        return "/common/find-id-pw";
     }
     
     
