604422ff63a6641a890fe23808690e7299c1d1af
yoonkimin <sked4824@gmail.com>
2025-03-28 17:45:30 +0900

    Feat(admin-news-letter.html) -> 뉴스레터 관리 페이지 html추가 -> 총구독자 가져오는 기능 추가 -> 다음 발송일 계산하는 기능 추가 + html에 띄우기까지 -> 발송 성공률 계산 하는 기능 추가 -> 구독자 목록에서 구독자 조회해오는 기능추가, 구독 해지 기능까지 추가

---
+++ src/main/java/kr/kro/bbanggil/email/dto/response/SubscriptionResponseDto.java
@@ -0,0 +1,14 @@
+package kr.kro.bbanggil.email.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class SubscriptionResponseDto {
+
+	private String email;
+    private String subscribeDate;
+	
+}
